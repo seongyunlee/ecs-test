51e6dfd543594c45a68fea930876876ea5bc4339
seongyunlee <private.seongyun@gmail.com>
2025-02-26 22:37:53 +0900

    task definition

---
+++ src/main/java/site/kkrupp/ecs_test/EcsTestApplication.java
@@ -0,0 +1,13 @@
+package site.kkrupp.ecs_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/kkrupp/ecs_test/EcsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kkrupp.ecs_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
